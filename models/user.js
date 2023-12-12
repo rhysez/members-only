45140dc8c81db13dd8a5cf89e5419b412156633f
rhysez <121772484+rhysez@users.noreply.github.com>
2023-12-12 20:56:21 +0000

    add new virtual property to UserSchema

--- models/user.js
+++ models/user.js
@@ -9,7 +9,19 @@ const UserSchema = new Schema({
 
     user_name: { type: String, required: true, maxlength: 100 },
 
-    password: { type: String, required: true, maxLength: 75 }
+    password: { type: String, required: true, maxLength: 75 },
+
+    membership_status: { type: String }
 });
+
+UserSchema.virtual('full_name', function() {
+    let fullName = '';
+
+    if (this.first_name && this.last_name) {
+        fullName = `${this.first_name} ${this.last_name}`
+    };
+
+    return fullName
+})
 
 modules.exports(mongoose.model('User', UserSchema));
